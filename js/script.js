642a12a366960f10f95b8c9db6c6b9d612f65639
curs3d <thecool603@gmail.com>
2021-02-21 18:49:58 +0300

    update script.js

--- js/script.js
+++ js/script.js
@@ -1,20 +1,37 @@
 "use strict";
 
-let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+let numberOfFilms = + prompt('Сколько фильмов вы уже посмотрели?', '');
 
 let personalMovieDB = {
-count: numberOfFilms,
-movies: {},
-actors: {},
-genres: [],
-privat: false
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
 };
-let a =  prompt('Один из последних просмотренных фильмов?', ''),
-    b = +prompt('На сколько оцените его?', ''),
-    c = prompt('Один из последних просмотренных фильмов?', ''),
-    d = +prompt('На сколько оцените его?', '');
 
-    personalMovieDB.movies[a] = b;
-    personalMovieDB.movies[c] = d;
+if (personalMovieDB.count < 10){
+    alert('Просмотренно довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30){
+    alert('Вы улассический зритель');
+} else if (personalMovieDB.count > 30){
+    alert('Вы киноман');
+} else {
+    alert('Произошла ошибка');
+}
 
-    console.log(personalMovieDB);
+for(let i = 0; i < 2; i++){
+    let a = prompt('Один из последних просмотренных фильмов?', ''),
+    b = prompt('На сколько оцените его?', '');
+
+    if(a != '' && b != '' && a != null && b != null && a.length < 51){
+        personalMovieDB.movies[a] = +b;
+        console.log('done');
+    } else {
+        alert('error');
+        i--;
+    }
+}
+
+
+console.log(personalMovieDB);
